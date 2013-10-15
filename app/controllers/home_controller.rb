cd54c6eacfe26702e887ed8d559165090e5c31bb
John Meeker <jmeeker516@gmail.com>
2013-10-14 20:38:41 -0700

    Base project added with devise and boostrap

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  def index
+    #@tweet = Tweet.last
+    #gon.tweet = @tweet
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StacksocialTwitter::Application.config.secret_token = 'd62c0b0b5188b689e9cc990b35d2de1ca05d97787d77422a496d462b770a1e5914c00c8a6f22801449e87f787bb3f6a2b87b5be53cb2cb997593e76852d4f8f6'
